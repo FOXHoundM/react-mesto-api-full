3728b7a97e55a9c0998fd78f322d96963ed3ddc3
FOXHoundM <4216623@gmail.com>
2023-01-11 19:46:18 +0300

    cors env

--- backend/helpers/token.js
+++ backend/helpers/token.js
@@ -1,11 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
+// const secretKey = NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret';
 
-const generateToken = (payload) => jwt.sign(payload, secretKey, { expiresIn: '7d' });
+const generateToken = (payload) => jwt.sign(payload,
+  NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+  { expiresIn: '7d' }
+);
 
 module.exports = {
-  secretKey,
+  // secretKey,
   generateToken,
 };
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
-const { secretKey } = require('../helpers/token');
+const { NODE_ENV, JWT_SECRET } = process.env;
+// const { secretKey } = require('../helpers/token');
 
 const handleAuthError = (res) => {
   res
@@ -20,7 +21,7 @@ module.exports = (req, res, next) => {
   let payload;
 
   try {
-    payload = jwt.verify(token, secretKey);
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
     return handleAuthError(res);
   }
