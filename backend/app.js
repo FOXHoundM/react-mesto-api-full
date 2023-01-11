aaa3d551324435e32903eae83a7a0b0c23b48fd4
FOXHoundM <4216623@gmail.com>
2023-01-11 19:14:06 +0300

    cors fix

--- backend/app.js
+++ backend/app.js
@@ -34,8 +34,8 @@ const allowedCors = [
 const corsOptions = {
   origin: allowedCors,
   optionSuccessStatus: 200,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
   credentials: true,
 };
 
